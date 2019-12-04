f4947c0cd281014ac1b9738cc8676a52f2e4ad51
luanlelis12 <luanlelis09@gmail.com>
2019-12-04 20:04:47 -0300

    sites

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
 
-    return 'oi'
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
